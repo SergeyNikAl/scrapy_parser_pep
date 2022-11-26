b6a3a5eede77132d445dcd88360b2f1245e22bb8
SergeyNikAl <cergei_nikulin@mail.ru>
2022-11-26 16:57:57 +0300

    fix middlewares

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -1,12 +1,4 @@
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 from scrapy import signals
-
-# useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
 
 
 class PepParseSpiderMiddleware:
